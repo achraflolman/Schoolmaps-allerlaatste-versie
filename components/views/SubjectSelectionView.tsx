931e1cadaaae75caa71be8f0d0b2371bb6604d2f
achraflolman <104624757+achraflolman@users.noreply.github.com>
2025-09-14 22:11:05 +0200

    feat: Add subject icons to Schoolmaps Pro
    Integrates a variety of Lucide icons to visually represent different school subjects, enhancing the user interface of the subject selection view. This change provides a more engaging and intuitive experience for students managing their academic subjects.

--- components/views/SubjectSelectionView.tsx
+++ components/views/SubjectSelectionView.tsx
@@ -1,6 +1,10 @@
 import React, { useMemo } from 'react';
 import type { AppUser } from '../../types';
-import { Folder } from 'lucide-react';
+import {
+    Folder, Globe, Calculator, Atom, FlaskConical, Dna,
+    ScrollText, AreaChart, Users, Languages, Code, Paintbrush,
+    Music, Dumbbell, Film
+} from 'lucide-react';
 
 interface SubjectSelectionViewProps {
   user: AppUser;
@@ -15,6 +19,32 @@ const SubjectSelectionView: React.FC<SubjectSelectionViewProps> = ({ user, t, tS
     const combined = new Set([...(user.selectedSubjects || []), ...(user.customSubjects || [])]);
     return Array.from(combined);
   }, [user.selectedSubjects, user.customSubjects]);
+
+  const subjectIcons = useMemo(() => ({
+    'aardrijkskunde': <Globe className={`w-12 h-12 mx-auto mb-2 ${getThemeClasses('text')}`} />,
+    'wiskunde': <Calculator className={`w-12 h-12 mx-auto mb-2 ${getThemeClasses('text')}`} />,
+    'natuurkunde': <Atom className={`w-12 h-12 mx-auto mb-2 ${getThemeClasses('text')}`} />,
+    'scheikunde': <FlaskConical className={`w-12 h-12 mx-auto mb-2 ${getThemeClasses('text')}`} />,
+    'biologie': <Dna className={`w-12 h-12 mx-auto mb-2 ${getThemeClasses('text')}`} />,
+    'geschiedenis': <ScrollText className={`w-12 h-12 mx-auto mb-2 ${getThemeClasses('text')}`} />,
+    'latijn': <ScrollText className={`w-12 h-12 mx-auto mb-2 ${getThemeClasses('text')}`} />,
+    'economie': <AreaChart className={`w-12 h-12 mx-auto mb-2 ${getThemeClasses('text')}`} />,
+    'maatschappijleer': <Users className={`w-12 h-12 mx-auto mb-2 ${getThemeClasses('text')}`} />,
+    'nederlands': <Languages className={`w-12 h-12 mx-auto mb-2 ${getThemeClasses('text')}`} />,
+    'engels': <Languages className={`w-12 h-12 mx-auto mb-2 ${getThemeClasses('text')}`} />,
+    'frans': <Languages className={`w-12 h-12 mx-auto mb-2 ${getThemeClasses('text')}`} />,
+    'duits': <Languages className={`w-12 h-12 mx-auto mb-2 ${getThemeClasses('text')}`} />,
+    'informatica': <Code className={`w-12 h-12 mx-auto mb-2 ${getThemeClasses('text')}`} />,
+    'kunst': <Paintbrush className={`w-12 h-12 mx-auto mb-2 ${getThemeClasses('text')}`} />,
+    'muziek': <Music className={`w-12 h-12 mx-auto mb-2 ${getThemeClasses('text')}`} />,
+    'lichamelijke_opvoeding': <Dumbbell className={`w-12 h-12 mx-auto mb-2 ${getThemeClasses('text')}`} />,
+    'ckv': <Film className={`w-12 h-12 mx-auto mb-2 ${getThemeClasses('text')}`} />,
+    'default': <Folder className={`w-12 h-12 mx-auto mb-2 ${getThemeClasses('text')}`} />
+  }), [getThemeClasses]);
+
+  const getIconForSubject = (subjectKey: string) => {
+      return subjectIcons[subjectKey as keyof typeof subjectIcons] || subjectIcons['default'];
+  };
 
   return (
     <div className="space-y-6 animate-fade-in">
@@ -34,7 +64,7 @@ const SubjectSelectionView: React.FC<SubjectSelectionViewProps> = ({ user, t, tS
               onClick={() => setCurrentSubject(subject)}
               className={`p-6 bg-white rounded-lg shadow-md font-semibold text-center hover:shadow-lg hover:-translate-y-1 transition-all duration-200 focus:outline-none focus:ring-2 ${getThemeClasses('ring')} ${getThemeClasses('text-strong')}`}
             >
-              <Folder className={`w-12 h-12 mx-auto mb-2 ${getThemeClasses('text')}`} />
+              {getIconForSubject(subject)}
               {tSubject(subject)}
             </button>
           ))}
